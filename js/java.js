62d6c98e1145cc6c7ede08f0aab7b7a3b5439ade
SajaSwalgah <55652548+SajaSwalgah@users.noreply.github.com>
2019-10-08 14:25:16 +0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -2,104 +2,157 @@
 
 var user = prompt(" Can You Please Tell Us Your Name ")
 alert("Welcome You Here " + user + " I wish You Enjoy Knowing Me..")
-var questions =[
-["My name is an Arabian name means when the night become calm and dark before the dorn..Did you know it?",
-" Great Job.. Yes my name is Saja",
-" It's OK.. Actually my name is Saja",
-"Try to know my name"
-],
-[
-"I should work with medical instroments in hospitals or with artificial organs.. Did you know what is my major?",
-" Great Job.. Yes I am a biomedical enginner",
-" I'm sure you were about to know it ... My major is biomedical engineering",
-"Try to know my major"
 
-
-],
-[
-"I started school in 2013 and graduated after 5 years.. Did you Know what year I've graduated?",
-" Great Job.. Yes I've graduated in 2018",
-" You are bad in math.. I've graduated in 2018 ",
-"Try to know my graduation year"
-
-],
-[
-" I've read a lot of books and my faviort auther is Dan brown.. Did you guess what is my hobby?",
-" Great Job.. my hobby is reading",
-" Really! My hobby is reading .. It's so easy",
-"Try to know my hobby"
-
-],
-[
-"My faviort color is the color of the sky and the sea.. Did you Know it?",
-" Great Job.. Yes my faviort color is blue",
-" You don't Know the color of the sky! My faviort color is blue",
-"Try to know my faviort color"
-
-]
-
-]
-
-
-for (var i=0; i<5; i++){
-var exactArray = questions[i];
-var promptQuestion = exactArray[0];
-var posetiveReply = exactArray[1];
-var negativeReply = exactArray[2];
-var defultReply = exactArray[3];
-var name = prompt(promptQuestion);
-promptQuestion = promptQuestion.toLowerCase();
-switch (name) {
-case "yes":
-case "y":
-alert(posetiveReply);
-break;
-
-
-case "no":
-case "n":
-alert(negativeReply);
-break;
-
-default :
-alert (defultReply) ;
-break;
-}}
-
-for (var age = 0; age <4; age++){
-
-    var old = prompt("Guess how old am I... You have 4 trials")
-    if (old ==24){
-        alert("Yes exactly!");
+var name = prompt("My name is an Arabian name means when the nigh become calm and dark before the dorn..Did you know it?")
+name = name.toLowerCase();
+switch (name ) { 
+    case "yes" :
+        alert(" Great Job.. Yes my name is Saja");
+        //console.log ("Great Job.. you guessed my name");
+        alert("Great Job.. you guessed my name");
         break;
-    } else if (old >24) {
-        alert ("too high");
-    } else if ( old< 24){
-        alert(" too low");
-    }
+    case "y" :
+         alert(" Great Job.. Yes my name is Saja");
+         //console.log ("Great Job.. you guessed my name");
+        alert("Great Job.. you guessed my name");
+         break;  
+    case "no" :
+        alert(" It\'s OK.. Actually my name is Saja");
+       // console.log ("You could not guess my name");
+        alert ("You could not guess my name");
+        break;
+    case "n" :
+        alert(" It\'s OK.. Actually my name is Saja");
+        //console.log ("You could not guess my name");
+        alert ("You could not guess my name");
+        break; 
+        default:
+        alert(" You don't want to Know my name?");
+        //console.log ("Try to know my name");
+        alert("Try to know my name");
+        break;
+        
 }
 
-var game = ["cat", 'dog', 'hours', 'eagle', 'eleghant', 'dolphin']
-var play = prompt("I love 6 different animals.. Can you guess one of them?");
-switch (play){
-    case "cat":
-    case "dog":
-    case "hourse":
-    case "eagle":
-    case "elephant":
-    case "dolphin" :
-        alert (" You are right.. My fav animals are: Cat, Dog, Hours, Eagle, Eleghant, Dolphin")
+
+var major = prompt("I should work with medical instroments in hospitals or with artificial organs.. Did you know what is my major?")
+major = major.toLowerCase();
+switch (major ) { 
+    case "yes" :
+        alert(" Great Job.. Yes I am a biomedical enginner");
+        //console.log ("Great Job.. you guessed my major");
+        alert("Great Job.. you guessed my major");
         break;
-   
-
-    default:
-        for (var times = 0; times <5 ; times++){
-            alert ( "You are wrong .. Try it again ")
-            prompt("Try again..");
-        }  
-        alert( " My fav animals are: Cat, Dog, Hours, Eagle, Eleghant, Dolphin ") 
+    case "y" :
+         alert(" Great Job.. Yes I am a biomedical enginner");
+         //console.log ("Great Job.. you guessed my major");
+        alert("Great Job.. you guessed my major");
+         break;  
+    case "no" :
+        alert(" I'm sure you were about to know it ... My major is biomedical engineering");
+        //console.log ("You could not guess my major");
+        alert("You could not guess my major");
+        break;
+    case "n" :
+        alert(" I'm sure you were about to know it ... My major is biomedical engineering");
+        //console.log ("You could not guess my major");
+        alert("You could not guess my major");
         break; 
+    default:
+        alert(" You don't want to Know my major? my major is biomedical engineering");
+        //console.log ("Try to know my major");
+        alert("Try to know my major");
+         break;
+}
 
+var year = prompt("I started school in 2013 and graduated after 5 years.. Did you Know what year I've graduated?")
+year = year.toLowerCase();
+switch (year ) { 
+    case "yes" :
+        alert(" Great Job.. Yes I've graduated in 2018");
+        //console.log ("Great Job.. you guessed my graduation year");
+        alert("Great Job.. you guessed my graduation year");
+        break;
+    case "y" :
+         alert(" Great Job.. Yes I've graduated in 2018");
+         //console.log ("Great Job.. you guessed my graduation year");
+        alert("Great Job.. you guessed my graduation year");
+         break;  
+    case "no" :
+        alert(" You are bad in math.. I've graduated in 2018 ");
+        //console.log ("You could not guess my graduation year");
+        alert("You could not guess my graduation year");
+        break;
+    case "n" :
+        alert(" You are bad in math.. I've graduated in 2018");
+        //console.log ("You could not guess my graduation year");
+        alert("You could not guess my graduation year");
+        break; 
+    default:
+        alert(" You don't want to Know my graduation year? ");
+        //console.log ("Try to know my graduation year");
+        alert("Try to know my graduation year");
+        break;
+}
+
+var hobby = prompt(" I've read a lot of books and my faviort auther is Dan brown.. Did you guess what is my hobby?")
+hobby = hobby.toLowerCase();
+switch (hobby ) { 
+    case "yes" :
+        alert(" Great Job.. my hobby is reading");
+        //console.log ("Great Job.. you guessed my hobby");
+        alert("Great Job.. you guessed my hobby");
+        break;
+    case "y" :
+         alert(" Great Job.. my hobby is reading");
+         //console.log ("Great Job.. you guessed my hobby");
+        alert("Great Job.. you guessed my hobby");
+         break;  
+    case "no" :
+        alert(" Really! My hobby is reading .. It's so easy");
+        //console.log ("You could not guess my hobby");
+        alert("You could not guess my hobby");
+        break;
+    case "n" :
+        alert(" Really! My hobby is reading .. It's so easy");
+        //console.log ("You could not guess my hobby");
+        alert("You could not guess my hobby");
+        break;
+        default:
+                alert(" You don't want to Know my hobby? ");
+                //console.log ("Try to know my hobby");
+        alert("Try to know my hobby");
+                break; 
+}
+
+var color = prompt("My faviort color is the color of the sky and the sea.. Did you Know it?")
+color = color.toLowerCase();
+switch (color ) { 
+    case "yes" :
+        alert(" Great Job.. Yes my faviort color is blue");
+       // console.log ("Great Job.. you guessed my faviort color");
+        alert("Great Job.. you guessed my faviort color");
+        break;
+    case "y" :
+         alert(" Great Job.. Yes my faviort color is blue");
+         //console.log ("Great Job.. you guessed my faviort color");
+        alert("Great Job.. you guessed my faviort color");
+         break;  
+    case "no" :
+        alert(" You don't Know the color of the sky! My faviort color is blue");
+        //console.log ("You could not guess my faviort color");
+        alert ("You could not guess my faviort color");
+        break;
+    case "n" :
+        alert(" You don't Know the color of the sky! My faviort color is blue");
+        //console.log ("You could not guess my faviort color");
+        alert ("You could not guess my faviort color");
+        break; 
+        default:
+                alert(" You don't want to Know my faviort color? ");
+              //  console.log ("Try to know my faviort color");
+        alert("Try to know my faviort color");
+                break;
 }
 
 alert("Thanks for being here " + user);
